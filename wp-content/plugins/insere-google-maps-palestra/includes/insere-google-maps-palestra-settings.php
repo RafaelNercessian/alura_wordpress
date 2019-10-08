77b379cfece46d85ff826c73f4dbcbda44bf0d36
RafaelNercessian <rafanercessian@gmail.com>
2019-10-08 14:20:37 -0300

    Adicionando data local

--- wp-content/plugins/insere-google-maps-palestra/includes/insere-google-maps-palestra-settings.php
+++ wp-content/plugins/insere-google-maps-palestra/includes/insere-google-maps-palestra-settings.php
@@ -81,3 +81,35 @@ function mostra_conteudo_cidade_pagina_admin(){
     echo '<input type="text" id="insere-local-palestra-cidade" name="insere-local-palestra-cidade" value="' . $input . '" />';
 }
 
+add_action('admin_init', 'insere_google_maps_datepicker');
+function insere_google_maps_datepicker(){
+    add_settings_field(
+        'insere-local-palestra-data',
+        'Data',
+        'mostra_conteudo_data_pagina_admin',
+        'local-palestra',
+        'insere-local-palestra-secao-1'
+    );
+    register_setting(
+        'local-palestra-settings',
+        'insere-local-palestra-data'
+    );
+}
+
+function mostra_conteudo_data_pagina_admin(){
+    $input = get_option( 'insere-local-palestra-cidade' );
+    echo '<input type="date" id="insere-local-palestra-data" name="insere-local-palestra-data[datepicker]" value="" class="example-datepicker" />';
+}
+
+add_action( 'admin_enqueue_scripts', 'enqueue_date_picker' );
+function enqueue_date_picker(){
+    wp_enqueue_script(
+        'field-date-js',
+        'Field_Date.js',
+        array('jquery', 'jquery-ui-core', 'jquery-ui-datepicker'),
+        time(),
+        true
+    );
+
+    wp_enqueue_style( 'jquery-ui-datepicker' );
+}
