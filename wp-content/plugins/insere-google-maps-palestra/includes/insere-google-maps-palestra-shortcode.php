b0720770ff12941c12c22c54409d7453443cd914
RafaelNercessian <rafanercessian@gmail.com>
2019-10-08 17:43:37 -0300

    Mudanças da hora até o momento

--- wp-content/plugins/insere-google-maps-palestra/includes/insere-google-maps-palestra-shortcode.php
+++ wp-content/plugins/insere-google-maps-palestra/includes/insere-google-maps-palestra-shortcode.php
@@ -4,8 +4,8 @@ function insere_google_maps_com_local($atts, $content = null)
 {
 
     $endereco = urlencode((get_option('insere-local-palestra-endereco')));
-    $cidade = urlencode((get_option('insere-local-palestra-endereco')));
-    return '<div class="mapouter"><div class="gmap_canvas"><iframe width="100%" height="500" id="gmap_canvas" src="https://maps.google.com/maps?q=' . $endereco .  $cidade . '&t=&z=13&ie=UTF8&iwloc=&output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe><a href="https://usave.co.uk">usave</a></div></div>';
+    $cidade = urlencode((get_option('insere-local-palestra-cidade')));
+    return '<div class="mapouter"><div class="gmap_canvas"><iframe width="100%" height="500" id="gmap_canvas" src="https://maps.google.com/maps?q=' . $endereco  .  $cidade . '&t=&z=13&ie=UTF8&iwloc=&output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe></div></div>';
 
 }
 
