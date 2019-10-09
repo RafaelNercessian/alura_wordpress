11c924e662adf0b0dc64d207250fc693ce069ebc
RafaelNercessian <rafanercessian@gmail.com>
2019-10-09 14:56:37 -0300

    Parece que tah funcionando

--- wp-content/plugins/insere-google-maps-palestra/includes/insere-google-maps-palestra-scripts.php
+++ wp-content/plugins/insere-google-maps-palestra/includes/insere-google-maps-palestra-scripts.php
@@ -4,5 +4,13 @@ add_action('wp_enqueue_scripts', 'insere_google_maps_palestra_css_scripts');
 
 function insere_google_maps_palestra_css_scripts()
 {
-    wp_enqueue_style('custom_css_google_maps',plugins_url() . '/insere-google-maps-palestra/css/google_maps_style.css');
+    wp_enqueue_style('custom_css_google_maps', plugins_url() . '/insere-google-maps-palestra/css/google_maps_style.css');
+    wp_enqueue_script('jquery', 'https://cdnjs.cloudflare.com/ajax/libs/jquery/1.8.3/jquery.min.js', array(), false, true);
+    wp_enqueue_script('countdown', plugins_url() . '/insere-google-maps-palestra/js/jquery.countdown.min.js', array(), false, true);
+
+    //passando data do evento
+    wp_register_script( 'calcula-data-evento', plugins_url() . '/insere-google-maps-palestra/js/calculate_countdown.js', array(), false, true );
+    $data = get_option('insere-local-palestra-data');
+    wp_localize_script( 'calcula-data-evento', 'data', $data );
+    wp_enqueue_script( 'calcula-data-evento' );
 }
--- wp-content/plugins/insere-google-maps-palestra/includes/insere-google-maps-palestra-shortcode.php
+++ wp-content/plugins/insere-google-maps-palestra/includes/insere-google-maps-palestra-shortcode.php
@@ -5,7 +5,18 @@ function insere_google_maps_com_local($atts, $content = null)
 
     $endereco = urlencode((get_option('insere-local-palestra-endereco')));
     $cidade = urlencode((get_option('insere-local-palestra-cidade')));
-    return '<div class="mapouter"><div class="gmap_canvas"><iframe width="100%" height="500" id="gmap_canvas" src="https://maps.google.com/maps?q=' . $endereco  .  $cidade . '&t=&z=13&ie=UTF8&iwloc=&output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe></div></div>';
+    return '<div class="mapouter">
+                 <div class="gmap_canvas">
+                    <iframe width="100%" height="500" id="gmap_canvas" src="https://maps.google.com/maps?q=' . $endereco . $cidade . '&t=&z=13&ie=UTF8&iwloc=&output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe>
+                 </div>
+             </div>
+             <h2 class="proximo-evento">Próximo evento</h2>
+             <div id="evento">
+                <div id="dias"></div>
+                <div id="horas"></div>
+                <div id="minutos"></div>
+                <div id="segundos"></div>
+             </div>';
 
 }
 
